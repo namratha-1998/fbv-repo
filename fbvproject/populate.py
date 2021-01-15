f7a2915772f67e9e097c35c5ca9be04921fb862b
namratha-1998 <namrathakr2016@gmail.com>
2021-01-15 10:39:54 +0530

    my -commit

---
+++ fbvproject/populate.py
@@ -0,0 +1,15 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbvproject.settings')
+django.setup()
+from faker import Faker
+from myApp.models import Employee
+f=Faker()
+def populate(n):
+ for i in range(n):
+        fno=f.random_int (min=1,max=20)
+        fname=f.name()
+        fsal=f.random_int(min=1000,max=20000)
+        faddr=f.address()
+        e=Employee.objects.get_or_create(eno=fno, ename=fname, esal=fsal, eaddr=faddr)
+        populate(20)
